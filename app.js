a14a44e51d9d17a120bae4b1cba3eca9a2d9abb0
Jens Andreasson <Jens Andreasson@umea.nti.se>
2017-01-23 15:38:01 +0100

    port fix

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('listening on *:3000');
+app.listen(process.env.PORT, function () {
+  console.log('listening on ' + process.env.PORT);
 });
