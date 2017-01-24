d6ea554fbd8d0a8a62a1e5063383e044a607b931
zvorak <jensandreasson77@gmail.com>
2017-01-24 20:36:18 +0100

    working localhost example

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/js/sw.js');
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
 }
 
 // presentation showHide
